ec85c65c1cb7d08fd641de97d88e9a6700b14670
LuizPauloDamaceno <luizpaulodamaceno@live.com>
2020-05-20 21:13:57 -0300

    add the 1958 MHz as boot clock

--- drivers/clk/qcom/clk-cpu-8976.c
+++ drivers/clk/qcom/clk-cpu-8976.c
@@ -1328,8 +1328,8 @@ static struct notifier_block clock_panic_notifier = {
 };
 
 static const unsigned long cci_boot_rate = 614400000;
-static const unsigned long a53ss_boot_rate = 1440000000;
-static const unsigned long a72ss_boot_rate = 1747200000;
+static const unsigned long a53ss_boot_rate = 1401600000;
+static const unsigned long a72ss_boot_rate = 1958400000;
 static int clock_cpu_probe(struct platform_device *pdev)
 {
 	int i, speed_bin, version, rc, cpu, mux_id, clks_sz;

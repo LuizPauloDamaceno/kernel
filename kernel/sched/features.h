4c7d73b59e2206da99ef9d680ea2576c020bf087
LuizPauloDamaceno <luizpaulodamaceno@live.com>
2020-05-20 23:49:14 -0300

    kernel: tune sched by correct efficiency factors and enable feature

--- kernel/sched/features.h
+++ kernel/sched/features.h
@@ -95,7 +95,7 @@ SCHED_FEAT(ARCH_POWER, true)
  * If enabled, this can be used to inform the scheduler about capacity
  * restrictions.
  */
-SCHED_FEAT(MIN_CAPACITY_CAPPING, false)
+SCHED_FEAT(MIN_CAPACITY_CAPPING, true)
 
 /*
  * Enforce the priority of candidates selected by find_best_target()
